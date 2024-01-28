18f33a1a26294f702777ac376823406966656a74
zumbeedoapp <131698403+zumbeedoapp@users.noreply.github.com>
2024-01-28 16:27:05 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zumbeedo-lp.github.io',
+  base: '/zumbeedo-lp',
   integrations: [tailwind()]
 });
